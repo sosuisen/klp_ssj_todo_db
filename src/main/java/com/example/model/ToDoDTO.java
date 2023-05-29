e42ac09190fa314319f8cccc7b7a3d695ae5a95d
sosuisen <hidekazu.kubota@gmail.com>
2023-05-30 01:48:02 +0900

    fix: order of annotations

--- src/main/java/com/example/model/ToDoDTO.java
+++ src/main/java/com/example/model/ToDoDTO.java
@@ -2,13 +2,12 @@ package com.example.model;
 
 import jakarta.ws.rs.FormParam;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Setter @Getter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ToDoDTO {
 	@FormParam("id")
 	private int id;
--- src/main/java/com/example/model/ToDos.java
+++ src/main/java/com/example/model/ToDos.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 
-@Named
 @RequestScoped
+@Named
 public class ToDos extends ArrayList<ToDoDTO> {
 
 }
